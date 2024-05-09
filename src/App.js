f7f3f2a41ed8a9836d0a03fab57e4dfcad4ce4bd
priyanshu2400 <priyanshujha024@gmail.com>
2024-05-09 17:14:57 +0530

    create react-app and tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="text-3xl m-4 p-4 w-full bg-pink-500">
+        hello
       </header>
     </div>
   );
